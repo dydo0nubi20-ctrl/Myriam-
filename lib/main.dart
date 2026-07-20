2149a3c7cd9540acba877f72d6e612a4fcea240e
dydo0nubi20-ctrl <dydo0nubi20@gmail.com>
2026-07-21 00:14:27 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'app/app.dart';
+import 'app/app_bootstrap.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations(const [
+    DeviceOrientation.portraitUp,
+  ]);
+
+  final container = await AppBootstrap().compose();
+
+  runApp(
+    UncontrolledProviderScope(
+      container: container,
+      child: const SetRizeApp(),
+    ),
+  );
+}
